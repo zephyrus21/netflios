d4afd50eb41040c9b2a9b30637b59eb9f053a1d0
zephyrus21 <pandey.piyus@gmail.com>
2022-11-21 14:36:59 +0530

    Initial Commit

---
+++ Netflios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflios
+//
+//  Created by Piyush Pandey on 21/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
